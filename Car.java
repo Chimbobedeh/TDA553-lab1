e027f413e9dc2b0352d9a6be52ba1c58fbc90f0b
Chimbobedeh <117650415+Chimbobedeh@users.noreply.github.com>
2022-11-10 16:12:09 +0100

    V1

---
+++ Car.java
@@ -0,0 +1,94 @@
+public abstract class Car implements Movable {
+    
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+    private Position position;
+
+    public Car (int nrDoors, double enginePower, double currentSpeed, Color color, String modelName) {
+        this.nrDoors = nrDoors
+        this.enginePower = enginePower
+        this.currentSpeed = currentSpeed
+        this.color = color
+        this.modelName = modelName
+        this.position = new Position(0, 0);
+        stopEngine();
+    }
+
+    abstract public double speedFactor();
+
+    public void incrementSpeed(double amount){
+	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
+    }
+
+    public void decrementSpeed(double amount) {
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+    }
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public void setColor(Color clr) {
+	    color = clr;
+    }
+
+    public void startEngine() {
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine() {
+	    currentSpeed = 0;
+    }
+
+    public void setTurboOn() {
+	    turboOn = true;
+    }
+
+    public void setTurboOff() {
+	    turboOn = false;
+    }
+
+    public void move() {
+        this.position.move(this.currentSpeed);
+    }
+
+    public void turnLeft() {
+        this.position.direction -= 1;
+        this.position.direction %= 4;
+        //.position.setXNegagtive();
+    }
+
+    public void turnRight() {
+        this.position.direction -= 1;
+        this.position.direction %= 4;
+        //this.position.setXPositive();
+    }
+    
+    // TODO fix this method according to lab pm
+    public void gas(double amount) {
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount) {
+        decrementSpeed(amount);
+    }
+}
---
+++ Position.java
@@ -0,0 +1,90 @@
+/*
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+*/
+
+public class Position {
+// x represents movement in x-plane and y represents movement in the y-plane
+    private int x, y;
+    private int direction;
+    private int[] directions = {0,1,2,3};
+
+    Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.direction = 0;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+	    this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void move(double factor) {
+        // if direction is neråt  --> y-= factor
+        // if direction is uppåt --> y += factor
+        // if direction is höger --> x += factor
+        // if direction is vänster --> x -= factor
+
+        // turnRight --> this.direction = Direction.RIGHT
+        // turnLeft --> this.direction = Direction.LEFT
+        // Should we %= 4 here?
+        switch (this.direction) {
+            case 0:
+                // UP
+                y += factor;
+            case 1:
+                // RIGHT
+                x += factor;
+            case 2:
+                // DOWN
+                y -= factor;
+            case 3:
+                // LEFT
+                x -= factor;
+        }
+        /*
+        if (y < 0){
+          this.y -= factor;
+          this.x -= factor
+
+        }
+        else if(y => 0){
+            this.y += factor;
+            this.x += factor;
+        }
+        this.x += factor;
+        this.y += factor;
+        */
+    }
+    
+    public void setXNegagtive() {
+        // if X is positive, negate it
+        if (this.x > 0) {
+            this.x *=  -1;
+        }
+    }
+    
+    public void setXPositive() {
+        // if X is negative, negate it
+        if (this.x < 0) {
+            this.x = -this.x;
+        }
+        this.x += 0;
+    }
+}
