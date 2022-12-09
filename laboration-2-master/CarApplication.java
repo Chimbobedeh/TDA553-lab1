9f43843b90b14fae1bfde454a56e2b9146328eed
Chimbobedeh <117650415+Chimbobedeh@users.noreply.github.com>
2022-12-09 15:05:33 +0100

    vvvvvv

--- laboration-2-master/CarApplication.java
+++ laboration-2-master/CarApplication.java
@@ -4,6 +4,7 @@ public class CarApplication {
     public static void main(String[] args) {
         CarModel model = new CarModel();
         CarController controller = new CarController(model);
+
         Vehicle volvo = new Volvo240();
         Vehicle saab = new Saab95();
         Vehicle scania = new Scania();
@@ -16,21 +17,6 @@ public class CarApplication {
         model.addVehicle(scania);
 
         CarView view = new CarView("CarSim 1.0", model, controller);
-
-        // DrawPanel drawPanel = new DrawPanel(0, 0);
-
-        // model.addVehicle(new Saab95());
-        // model.addVehicle(new Scania());
-
-        // model.addVehicle(DrawableVehicleFactory.createSaab());
-        // model.addVehicle(DrawableVehicleFactory.createScania());
-        /*
-         * 
-         * drawPanel.add(DrawableVehicleFactory.createSaab(saab));
-         * drawPanel.add(DrawableVehicleFactory.createScania(scania));
-         * drawPanel.add(DrawableVehicleFactory.createVolvo(volvo));
-         */
-
         model.addObserver(view);
         model.start();
     }
