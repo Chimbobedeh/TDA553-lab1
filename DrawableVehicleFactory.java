fb540dbbd38913bdc752e238ea7a19a611c20a76
Chimbobedeh <117650415+Chimbobedeh@users.noreply.github.com>
2022-12-09 13:43:45 +0100

    DrawableVehicleFactory

--- DrawableVehicleFactory.java
+++ DrawableVehicleFactory.java
@@ -1,21 +1,26 @@
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
-import javax.swing.*;
-
-
 
 public class DrawableVehicleFactory {
     public static DrawableVehicle createSaab(Vehicle car) {
-        Image saabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab94.jpg"));
-        return DrawableVehicle(saabImage, car);
-    }
-
-    public static DrawableVehicle createVolvo(Vehicle car) {
-        Image volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
+        BufferedImage volvoImage = loadImage("pics/Saab94.jpg");
         return new DrawableVehicle(volvoImage, car);
     }
 
+    public static DrawableVehicle createVolvo(Vehicle car) {
+        BufferedImage volvoImage = loadImage("pics/Volvo240.jpg");
+        return new DrawableVehicle(volvoImage, car);
+    }
+
+    private static BufferedImage loadImage(String path) {
+        BufferedImage image = null;
+        try { // Print an error message in case file is not found with a try/catch block
+            image = ImageIO.read(DrawPanel.class.getResourceAsStream(path));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return image;
+    }
 }
