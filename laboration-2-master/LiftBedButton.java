39eee908aa9853479f030c1a83f3fe6245578b1c
Eric <eric.lundin@skola.vaxjo.se>
2022-12-15 15:54:37 +0100

    Refactor

--- laboration-2-master/LiftBedButton.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import java.util.List;
-
-public class LiftBedButton extends JButton {
-    LiftBedButton(List<Vehicle> vehicles) {
-        super("Scania Lift Bed");
-    }
-}
